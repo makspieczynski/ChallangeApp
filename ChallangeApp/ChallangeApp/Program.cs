934457a71959304846f3ab9dc86099a722d73153
Maks Pieczyński <maksymilian.pieczynski@op.pl>
2023-06-10 07:23:32 +0200

    zad dzień 3

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,33 +1,18 @@
 ﻿using System.ComponentModel.Design;
 
-int number = 45947512;
-string numberAsString = number.ToString();
-char[] letters = numberAsString.ToCharArray();
+var name = "Ewa";
+bool woman = true;
+var age = 33;
 
-int[] counter = new int[10];
-
-for (int i = 0; i <= 9; i++)
+if (woman == true && age < 30)
 {
-    counter[i] = 0;
+    Console.WriteLine("Kobieta poniżej 30 lat");
 }
-
-foreach (char letter in letters)
+else if (woman == true && name == "Ewa" && age == 33)
 {
-    if (letter == '0') counter[0]++;
-    else if (letter == '1') counter[1]++;
-    else if (letter == '2') counter[2]++;
-    else if (letter == '3') counter[3]++;
-    else if (letter == '4') counter[4]++;
-    else if (letter == '5') counter[5]++;
-    else if (letter == '6') counter[6]++;
-    else if (letter == '7') counter[7]++;
-    else if (letter == '8') counter[8]++;
-    else if (letter == '9') counter[9]++;
+    Console.WriteLine("Ewa lat 33");
 }
-
-Console.WriteLine("Liczba ->" + number);
-
-for (int i = 0; i < 10; i++)
+if (woman == false && age < 18)
 {
-    Console.WriteLine("Cyfra" + i + "->" + counter[i]);
+    Console.WriteLine("niepełnoletni mężczyzna");
 }
